2d9508e24bb4fd1dae8bebe82221504b131ba39e
Liz3 <accs@liz3.net>
2024-09-07 01:02:01 +0200

    update macos app generation script

--- tests/create_app.sh
+++ tests/create_app.sh
@@ -6,7 +6,7 @@ mkdir -p WeatherTest.app/Contents/assets/build
 cp ../tests/info.plist WeatherTest.app/Contents/Info.plist
 cp ../tests/location.du WeatherTest.app/Contents/assets/build
 cp libdictu-ui.dylib WeatherTest.app/Contents/assets/build
-cp /Users/liz3/Projects/Dictu/dictu WeatherTest.app/Contents/assets
+cp $(realpath $(which dictu)) WeatherTest.app/Contents/assets
 cp ../mod.du WeatherTest.app/Contents/assets
 cp ../tests/weathertest.sh WeatherTest.app/Contents/MacOS/WeatherTest
 chmod +x WeatherTest.app/Contents/MacOS/WeatherTest
